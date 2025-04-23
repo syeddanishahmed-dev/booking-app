0dd236d58bde36273f1fbe1f3521dda2d2f5fb74
Syed Danish Ahmed <syeddanishahmed.dev.com>
2025-04-23 21:06:42 +0500

    changes

--- routes/hotels.js
+++ routes/hotels.js
@@ -11,15 +11,7 @@ const router = express.Router();
 //CREATE
 router.post("/", async (req,res)=>{
 
-const newHotel = new Hotel(req.body);    
-    
-     
-    try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel) ;
-  } catch (error) {
-    res.status(500).json(error);
-  }  
+
 });
 //UPDATE
 router.put("/:id", async (req,res)=>{
