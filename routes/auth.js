5278fa720010ce39612eb345baa1b601ca3c257b
syeddanishahmed-dev <syeddanishahmed.dev@gmail.com>
2025-05-05 21:26:24 +0500

    changing

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ import { register } from "../controllers/auth.js"
 const router = express.Router();
 
 
-router.get("/register", register)
+router.post("/register", register)
 
 export default router;
