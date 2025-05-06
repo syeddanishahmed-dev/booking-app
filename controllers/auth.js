32f5aee758a24d00abfb3505d5fb6c9a0d67321c
syeddanishahmed-dev <syeddanishahmed.dev@gmail.com>
2025-05-06 22:00:51 +0500

    changing

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,7 +32,9 @@ export const login = async (req,res,next)=>{
        if(!isPasswordCorrect) 
         return next (createError(400,"wrong password or user name!"));
 
-        res.status(200).json(user)
+
+       const {password, isAdmin, ...otherDetails} = user._doc;
+        res.status(200).json({...otherDetails});
     }catch(err){
         next(err);
     }
